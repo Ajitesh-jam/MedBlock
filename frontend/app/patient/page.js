18c62d0c265036d02747f1130c54df5247d40ba8
Ajitesh-jam <ajitesh.jam@kgpian.iitkgp.ac.in>
2024-12-15 19:06:24 +0530

    Update page.js

--- frontend/app/patient/page.js
+++ frontend/app/patient/page.js
@@ -1,4 +1,4 @@
-
+{
 // 'use client'
 // import Layout from "@/components/layout/Layout"
 // import Link from "next/link"
@@ -299,9 +299,7 @@
 //         </>
 //     )
 // }
-
-
-
+}
 
 'use client'
 import Layout from "@/components/layout/Layout"
