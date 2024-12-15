a63d83d5664accd12e4dc619d03848bd0561eb30
Ajitesh-jam <ajitesh.jam@kgpian.iitkgp.ac.in>
2024-12-15 23:41:00 +0530

    patient page and navigateion ,funtioning prototype done

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+
+module.exports = {
+    async rewrites() {
+        return [
+            {
+                source: '/api/proxy/:path*',
+                destination: 'https://olive-defiant-ox-42.mypinata.cloud/:path*',
+            },
+        ];
+    },
+};
